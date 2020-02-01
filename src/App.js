43fed788d78b220fa0bae49ce939cdad388b891c
jmaysrow <maysrowlandjm@mymail.vcu.edu>
2020-02-01 13:45:18 -0800

    Added call to api from page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={}
+  }
+  render() {
+    const { response } = this.state
+    const apiMessage = response || "waiting ..."
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            /api/hello response = {apiMessage}
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch(window.location.href+"/api/hello")
+      .then(v => v.json())
+      .then(v => {
+        this.setState({ response: v.response })
+      })
+  }
 }
 
 export default App;
